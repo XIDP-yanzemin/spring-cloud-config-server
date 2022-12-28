f0de8b9e86693e0ae4d055a9955fc35cba796547
josieyan123 <josieyan123@gmail.com>
2022-12-28 21:40:10 +0800

    refactor: update config

--- src/main/java/com/practice/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ src/main/java/com/practice/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableEurekaClient
 public class SpringCloudConfigServerApplication {
 
     public static void main(String[] args) {
